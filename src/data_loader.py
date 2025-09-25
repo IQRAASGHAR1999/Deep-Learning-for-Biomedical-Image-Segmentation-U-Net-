c4a563c1b2630e9affa0cfdab60da18dbd0f1b6d
Iqra Asghar <114947104+IQRAASGHAR1999@users.noreply.github.com>
2025-09-25 22:41:35 +0500

    Create data_loader.py

---
+++ src/data_loader.py
@@ -0,0 +1,24 @@
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+import os
+
+class SegmentationDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = os.listdir(image_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.image_dir, self.images[idx])
+        mask_path = os.path.join(self.mask_dir, self.images[idx])
+        image = Image.open(img_path).convert("L")
+        mask = Image.open(mask_path).convert("L")
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
+        return image, mask
